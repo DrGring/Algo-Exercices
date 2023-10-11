25d36c360bb34295ad54e5906c907aac3b6bb43c
RenéDEK <gringok360@gmail.com>
2023-10-11 18:00:03 +0000

    Algorithme qui déclare et remplisse un tableaux de 7valeurs numériques en les mettant toutes à zéro

--- exercises6-1.js
+++ exercises6-1.js
@@ -1,7 +1,11 @@
 // Algorithme qui déclare et remplisse un tableaux de 7valeurs numériques en les mettant toutes à zéro 
 let tableaux = [];
+let valeurs = ['1','2','3','4','5','6','7','8']
 
  for(let i=0; i<7; i++){
-    console.log(i)
+
+   tableaux[i]=valeurs[i];
+   //  console.log(i)
     // console.log(tableaux[i]);
  }
+ console.log(tableaux)
