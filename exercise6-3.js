c50e6f1ff9429d2d6b129e5a9f5f9fdcad4d2fb9
RenéDEK <gringok360@gmail.com>
2023-10-11 17:01:04 +0000

    Envois de la branche 4

--- exercise6-3.js
+++
@@ -1,5 +0,0 @@
-// Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur 
-let tableaux = [] ;
-for (let i = 0; i < 9; i++) {
-   console.log("Saisir la note N°" + (i + 1) + ":");
-}
