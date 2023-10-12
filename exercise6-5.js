12ba99469ba197fa21f12a17608c1e3b181f851d
RenéDEK <gringok360@gmail.com>
2023-10-12 09:29:07 +0000

    Ce code affiche les valeur de 1 à 11 en y ajoutant +2 a chaques élément précédent

--- exercise6-5.js
+++ exercise6-5.js
@@ -7,3 +7,8 @@ for (let k = 1; k <6; k++) {
 for (let i = 0; i <6; i++) {
     console.log(alpha[i]);    
 }
+//crée un tableau vide appelé alpha,
+//affecter la valeur 1 à alpha[0], 
+//En utilisant une boucle for, on parcours les indices de 1 à 5 (inclus) du tableau alpha. Et a chaque boucle on calcules la valeur de alpha[k] en ajoutant 2 à la valeur de l'élément précédent.
+
+//utilisons une autre boucle for pour parcourir les indices de 0 à 5 du tableau alpha 
