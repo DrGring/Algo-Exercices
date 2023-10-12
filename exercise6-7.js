609b894bdd0027c9099472372a1301f1fdd1f4e9
RenéDEK <gringok360@gmail.com>
2023-10-12 11:06:34 +0000

    commit de la branche 6.7

---
+++ exercise6-7.js
@@ -0,0 +1,6 @@
+// Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur  (Exercise de la branchce Exo6.3)
+let tableaux = [10, 20, 15, 2, 5, 14, 12, 9, 36] ;
+
+for (let i = 0; i < 9; i++) {
+   console.log("Saisir la note N°" + (i + 1) + ":");
+}
