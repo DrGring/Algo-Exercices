9b2190c235890d794b5b6af93914bfd5bcaf3f10
RenéDEK <gringok360@gmail.com>
2023-10-13 11:32:44 +0000

    fixage de l'affichage de la note  saisis

--- exercise6-3.js
+++ exercise6-3.js
@@ -1,6 +1,6 @@
 // Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur 
-let tableaux = [10, 20, 15, 2, 5, 14, 12, 9, 36] ;
+let tableaux = [9,2,2,4,5,25,6,4,7];
 
 for (let i = 0; i < 9; i++) {
-   console.log("Saisir la note N°" + (i + 1) + ":");
+   console.log("Saisis la note " + (i + 1) + ": " +tableaux[i]);
 }
