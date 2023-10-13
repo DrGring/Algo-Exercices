d7ceb58c7e9274b64497558ba18c604e934500fc
RenéDEK <gringok360@gmail.com>
2023-10-13 10:38:53 +0000

    fixing of the prompt because it not define in node.js

--- exercise6-8.js
+++ exercise6-8.js
@@ -3,28 +3,44 @@
 // que l'utilisateur doit savoir le nombre de valeur a saisir et l'effectuera ensuite  
 // le programme affichera le nombre de valeur négatives et positives 
 
-// let monTableau = [];
-// let messageValeurs = ("Veuillez saisir le nombres de valeurs a saisir : ");
-// let SaisisValeur = parseInt(messageValeurs);
+let monTableau = [];
+const readline = require('readline');
+const interfaceLecture = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// let valeursPositives=0;
-// let valeursNegatives=0;
+interfaceLecture.question("Veuillez saisir le nombres de valeurs a saisir : ", function (reponse) {
+    console.log("Vous avez saisis : " + reponse);
+    interfaceLecture.close();
+});
 
-// for (let i = 0; i < SaisisValeur; i++) {
+let valeursPositives = 0;
+let valeursNegatives = 0;
 
-//     const valeur = parseFloat(("Veuillez entrer la valeur " +(i+1)+ ":" ));
 
-//     if (!isNaN(valeur)) {
-//         if (valeur>0) {
-//             valeursPositives++;
-//         }else if (valeur<0) {
-//             valeursNegatives++
-//         }
-//         monTableau.push(valeur);
-//     }
+/* let messageValeurs = ("Veuillez saisir le nombres de valeurs a saisir : ");
+let SaisisValeur = parseInt(messageValeurs);
 
-// }
+let valeursPositives=0;
+let valeursNegatives=0;
 
-// console.log("Les valeurs saisies : " + monTableau.join(", "));
-// console.log("Nombre de valeurs positives : " + valeursPositives);
-// console.log("Nombre de valeurs négatives : " + valeursNegatives);
+for (let i = 0; i < SaisisValeur; i++) {
+
+    const valeur = parseFloat(("Veuillez entrer la valeur " +(i+1)+ ":" ));
+
+    if (!isNaN(valeur)) {
+        if (valeur>0) {
+            valeursPositives++;
+        }else if (valeur<0) {
+            valeursNegatives++
+        }
+        monTableau.push(valeur);
+    }
+
+}
+
+console.log("Les valeurs saisies : " + monTableau.join(", "));
+console.log("Nombre de valeurs positives : " + valeursPositives);
+console.log("Nombre de valeurs négatives : " + valeursNegatives);
+ */
