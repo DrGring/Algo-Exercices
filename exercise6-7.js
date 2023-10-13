34e4f74c28636f83c0035c6af989b798178792a1
RenéDEK <gringok360@gmail.com>
2023-10-13 11:59:08 +0000

    Algo qui déclare un tableaux disposant des valeurs de  09 n otes que ainsi saisis  l'utilisateur  (Exercise de la branchce Exo6.3)

--- exercise6-7.js
+++ exercise6-7.js
@@ -1,10 +1,9 @@
 // Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur  (Exercise de la branchce Exo6.3)
-let tableaux = [10, 20, 15, 2, 5, 14, 12, 9, 36] ;
+let tableaux = [9,2,2,4,5,25,6,4,7] ;
 
-let i;
 for (let i = 0; i < 9; i++) {
-   console.log("Saisir la note N°" + (i + 1) + ":");
-   console.log(tableaux[i]);
+   console.log("Saisir la note N°" + (i + 1) + ": " + tableaux[i]);
+
 }
 //le code qui permet de calculer la somme a afficher 
 let somme = 0;
@@ -14,7 +13,7 @@ for (let i = 0; i <tableaux.length; i++) {
 
     console.log("Les notes saisies sont :");
 for (let i = 0; i < 9; i++) {
-    console.log(tableaux[i]);    
+    console.log(tableaux[i]);   
 }
 
 //Ecivons la fin pour que le calcul des notes soit éffectuées a l'écran
