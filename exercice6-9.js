0090d67a90f844c9b3de696ddfe73df0a99f77e4
RenéDEK <gringok360@gmail.com>
2023-10-16 16:30:58 +0000

    Saisis des valeurs ds un tableau et sommes des valeurs

--- exercice6-9.js
+++ exercice6-9.js
@@ -1,6 +1,7 @@
-let tableauValeur = []
-let valPosi = []
-let valNeg = []
+let tableauValeur = [];
+let valPosi = [];
+let valNeg = [];
+let somme = 0;
 
 for (let i = 0; i < 9; i++) {
     const valeur = prompt("Saisisez la valeur N°" +(i+1)+ ":" );
@@ -16,10 +17,14 @@ for (let i = 0; i < 9; i++) {
         console.log("Vous aviez Saisis une valeurs null");
     }
 }   
+for (let i = 0; i <tableauValeur.length; i++) {
+  somme += /* parseFloat */(tableauValeur[i]);
+}
 
 console.log(tableauValeur);
 console.log("les valeurs positives : " + valPosi);
 console.log("le(s) valeur(s) négatifs(ves) : " + valNeg);
+console.log("la somme est de : " +somme) ;
 
 alert(`Les valeurs saisis sont : ${tableauValeur}`);
 alert(`Les valeurs négatifs disctincts sont : ${valNeg} `)
