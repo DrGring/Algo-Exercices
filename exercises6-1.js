a0bc5fe588241bf6513706ddfca3540c2d66f434
RenéDEK <gringok360@gmail.com>
2023-10-17 09:54:10 +0000

    correction de l'exercises 6.1 en redéclarant un tableau vide sans valeurs et en les initialisant toutes à zéro

--- exercises6-1.js
+++ exercises6-1.js
@@ -1,11 +1,9 @@
-// Algorithme qui déclare et remplisse un tableaux de 7valeurs numériques en les mettant toutes à zéro 
-let tableaux = [];
-let valeurs = ['1','2','3','4','5','6','7','8']
+// Algorithme qui déclare et remplis un tableau de 7valeurs numériques en les mettant toutes à zéro 
+let tableaux  = [];
 
  for(let i=0; i<7; i++){
 
-   tableaux[i]=valeurs[i];
-   //  console.log(i)
-    // console.log(tableaux[i]);
+  tableaux.push(0)
+  
  }
  console.log(tableaux)
