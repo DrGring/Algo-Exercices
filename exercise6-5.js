833ed17a8833563436f97829fc776213817785d5
RenéDEK <gringok360@gmail.com>
2023-10-17 16:01:33 +0000

    L'algorithme affiche 6 nombres impairs en affectant la valeur 1 à 0

--- exercise6-5.js
+++ exercise6-5.js
@@ -1,14 +1,15 @@
-const alpha = [];
-
+let  alpha = [];
 alpha[0]=1;
+
 for (let k = 1; k <6; k++) {
     alpha[k]=alpha[k-1]+2;
 }
 for (let i = 0; i <6; i++) {
-    console.log(alpha[i]);    
+    console.log(alpha[i]);
 }
 //crée un tableau vide appelé alpha,
 //affecter la valeur 1 à alpha[0], 
-//En utilisant une boucle for, on parcours les indices de 1 à 5 (inclus) du tableau alpha. Et a chaque boucle on calcules la valeur de alpha[k] en ajoutant 2 à la valeur de l'élément précédent.
+//En utilisant une boucle for, on parcours les indices de 1 à 5 (inclus) du tableau alpha. 
+// Et a chaque boucle on calcules la valeur de alpha[k] en ajoutant 2 à la valeur de l'élément précédent.
 
 //utilisons une autre boucle for pour parcourir les indices de 0 à 5 du tableau alpha 
