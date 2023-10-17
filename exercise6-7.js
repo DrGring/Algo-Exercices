edea242fe2adcea9a18b2171d557d53ed58b1532
RenéDEK <gringok360@gmail.com>
2023-10-17 16:43:41 +0000

    Algorithme qui permet a l'utilisateur de saisir des notes inscits dans le tableau notes et qui après la saisie des notes affichées, le programme calcule la moyenne des notes obtenues

--- exercise6-7.js
+++ exercise6-7.js
@@ -1,21 +1,23 @@
-// Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur  (Exercise de la branchce Exo6.3)
-let tableaux = [9,2,2,4,5,25,6,4,7] ;
+// Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur(confère Exo6.3 ) 
+
+let notes = [9, 1, 10, 8, 15, 25, 6, 4, 7];
 
 for (let i = 0; i < 9; i++) {
-   console.log("Saisir la note N°" + (i + 1) + ": " + tableaux[i]);
-
+   const saisis = prompt("Saisis la note N°" + (i + 1) + ": " +notes[i]);
+   console.log(`Vous avez saisis ${saisis}`);
 }
-//le code qui permet de calculer la somme a afficher 
-let somme = 0;
-for (let i = 0; i <tableaux.length; i++) {
-  somme += tableaux[i];
+
+//le code qui permet de calculer la Moyenne a afficher 
+let moyenne = 0;
+for (let i = 0; i <notes.length; i++) {
+  moyenne += (notes[i])/9;
 }
 
     console.log("Les notes saisies sont :");
 for (let i = 0; i < 9; i++) {
-    console.log(tableaux[i]);   
+    console.log(notes[i]);   
 }
 
-//Ecivons la fin pour que le calcul des notes soit éffectuées a l'écran
-console.log(`la somme des notes est de : ${somme} `) ;
+//Ecivons la fin pour que le calcul des moyennes soit éffectuées a l'écran
+alert = prompt(`la moyenne est de : ${moyenne} `) ;
 
