897e088ac17f46a89ee35abef31308d58c2f9c8b
RenéDEK <gringok360@gmail.com>
2023-10-17 15:09:41 +0000

    refixing de l'algorithme qui dispose d'un tableau de 09 notes que  l'utilisateur va saisir

--- exercise6-3.js
+++ exercise6-3.js
@@ -1,6 +1,7 @@
 // Algo qui déclare un tableaux disposant des valeurs de  09 notes que ainsi saisis  l'utilisateur 
-let tableaux = [9,2,2,4,5,25,6,4,7];
+let tableaux = [9, 2, 2, 4, 5, 25, 6, 4, 7];
 
 for (let i = 0; i < 9; i++) {
-   console.log("Saisis la note " + (i + 1) + ": " +tableaux[i]);
+   const saisis = prompt("Saisis la note N°" + (i + 1) + " :" +tableaux[i]);
+   console.log(`Vous avez saisis ${saisis}`);
 }
