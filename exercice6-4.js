e79879468d7ad892c5e2132edd7da7d2f660f73e
RenéDEK <gringok360@gmail.com>
2023-10-17 15:21:43 +0000

    Ce code Fait la copie des valeurs du tableau empty et le colle dans le tableau notEmpty jusqu'a la cinquième valeurs

--- exercice6-4.js
+++ exercice6-4.js
@@ -1,9 +1,9 @@
-let tableau = [];
-const beta = ['0', '2', '3', '4', '5', '6'];
+let notEmpty = [];
+const empty = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 for (let i = 0; i <5; i++) {
-    tableau[i]= beta[i];  
+   notEmpty[i]= empty[i];  
 }
-console.log(tableau);
+console.log(notEmpty);
 
-//Le code copie les élements de la constante beta et le compte dans le tableau jusqu'a 5 fois
+//Le code copie les valeurs du tableau empty et le colle dans le tableau notEmpty jusqu'a la cinquième valeurs 
