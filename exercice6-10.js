0a33369bfe4b163211c82be0c10477f1b6642174
RenéDEK <gringok360@gmail.com>
2023-10-19 15:49:25 +0000

    Commit de l'agorithme constituant un tableaux de deux tableaux  de même longueur préalablement saisis, ainsi le nouveau tableau saisi sera la somme  des éléments des deux tableaux de départ

--- exercice6-10.js
+++
@@ -1,8 +0,0 @@
-// Algo constituant un tableaux de deux tableaux  de même longueur préalablement saisis. 
-// Ainsi le nouveau tableau saisi sera la somme  des éléments des deux tableaux de départ
-
-let tableau1 = ['4', '8', '7', '9', '1 ', '5', '4', '6'];
-let tableau2 = ['7', '6', '5', '2', '1 ', '3', '7', '4'];
-
-let tableau3 = [];
-
