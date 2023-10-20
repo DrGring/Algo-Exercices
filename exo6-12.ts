6c24d2d17443000717a77167da81eaebd899b865
RenéDEK <gringok360@gmail.com>
2023-10-20 15:20:58 +0000

    Dernière commit de l'algo qui permet à l'utilisateur de saisir une valeur numérique quelconque  et ensuite affichcer ses valeurs +1 dans le nouveau tableau

--- exo6-12.ts
+++ exo6-12.ts
@@ -1,44 +1,13 @@
 //Ecrire un algo qui permet à l'utilisateur de saisir une valeur numérique quelconque  et ensuite affichcer ses valeurs +1 dans le nouveau tableau 
 let valeuraSaisir: number[] = []
-let newTableauValeurSaisis: number[] = []
+let NOT_CHANGE: number = 4;
 
+for (let i = 0; i < NOT_CHANGE; i++) {
+  const valeurSaisieaParser: string | null = prompt("Saisisez la valeur N°" + (i + 1) + ": ");
 
+  if (valeurSaisieaParser/*  !== null */) {
 
-
-
-
-
-/* let tab1: number[] = [4, 8, 12];
-let tab2: number[] = [3, 6, 0];
-
-let sum = 0;
-
-for (let i = 0; i < tab1.length; i++) {
-  for (let j = 0; j < tab2.length; j++) {
-    sum += tab1[i] * tab2[j];
+    const valeurSaisieParser: number = parseInt(valeurSaisieaParser)/* +valeurSaisieaParser */;
+    valeuraSaisir.push(valeurSaisieParser + 1);
   }
-}
-  
-console.log('sum : ', sum)
-let tableau1: number[] = [4, 8, 12];
-let tableau2: number[] = [3, 6, 0];
-
-let schtroumpf: number = 0;
-let sumTableau1: number = 0;
-let sumTableau2: number = 0;
-
-
-
-for (let i = 0; i < tableau1.length; i++) {
-  sumTableau1 += tableau1[i];
-}
-
-for (let i = 0; i < tableau2.length; i++) {
-  sumTableau2 += tableau2[i];
-}
-schtroumpf = sumTableau1 * sumTableau2;
-
-console.log("sumTableau1: ", sumTableau1);
-console.log("sumTableau2: ", sumTableau2);
-console.log("schtroumpf: ", schtroumpf); */
-
+}console.log(valeuraSaisir);
