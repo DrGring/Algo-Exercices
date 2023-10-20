a96a0d7deccabd0ad46c15f9e444acb78e4dc7dd
RenéDEK <gringok360@gmail.com>
2023-10-20 13:17:08 +0000

    Premier commit de l'exercice 6.12

---
+++ exo6-12.ts
@@ -0,0 +1,33 @@
+let tab1: number[] = [4, 8, 12];
+let tab2: number[] = [3, 6, 0];
+
+let sum = 0;
+
+for (let i = 0; i < tab1.length; i++) {
+  for (let j = 0; j < tab2.length; j++) {
+    sum += tab1[i] * tab2[j];
+  }
+}
+  
+console.log('sum : ', sum)
+let tableau1: number[] = [4, 8, 12];
+let tableau2: number[] = [3, 6, 0];
+
+let schtroumpf: number = 0;
+let sumTableau1: number = 0;
+let sumTableau2: number = 0;
+
+
+
+for (let i = 0; i < tableau1.length; i++) {
+  sumTableau1 += tableau1[i];
+}
+
+for (let i = 0; i < tableau2.length; i++) {
+  sumTableau2 += tableau2[i];
+}
+schtroumpf = sumTableau1 * sumTableau2;
+
+console.log("sumTableau1: ", sumTableau1);
+console.log("sumTableau2: ", sumTableau2);
+console.log("schtroumpf: ", schtroumpf);
